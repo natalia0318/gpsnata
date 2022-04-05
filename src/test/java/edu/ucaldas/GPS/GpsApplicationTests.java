99facb6dc5931d90ecb8c91d6e91bdcfbd481ffd
natalia0318 <nataliacuervocastano@gmail.com>
2022-04-05 18:48:59 -0500

    Primer commit

---
+++ src/test/java/edu/ucaldas/GPS/GpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.GPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
